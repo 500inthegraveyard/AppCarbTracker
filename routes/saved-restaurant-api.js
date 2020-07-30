2221eff70c8697721622360d95a5289e99928960
500inthegraveyard <hoostasr@gmail.com>
2020-07-29 19:24:01 -0500

    push after trilogy help session,working object route

--- routes/saved-restaurant-api.js
+++ routes/saved-restaurant-api.js
@@ -3,17 +3,102 @@ var db = require("../models");
 
 module.exports = function(app) {
     app.get("/api/savedrestaurant", function(req, res) {
-      db.Restaurant.findAll({
-        include: [db.Food]
-      }).then(function(results) {
-        res.json(results);
-      })
-      .catch(function(err){
-          res.json(err);
+      db.Restaurant.findAll({include: [
+        {
+          model: db.Food,
+        }
+      ]
+    }).then(Restaurant => {
+      const resObj = Restaurant.map( ({dataValues}) => {
+        //desired object
+        // {
+        //     name: dataValues.name,
+        //     food: [
+        //         {
+        //             meal: "",
+        //             netCarbs: 0
+        //         }
+        //     ]
+        // }
+        return {
+            name: dataValues.name,
+            food: getFood(dataValues.Food)
+        }
+        // //tidy up the user data
+        // return Object.assign(
+        //   {},
+        //   {
+        //     name: Restaurant.name.map(post => {
+
+        //       //tidy up the post data
+        //       return Object.assign(
+        //         {},
+        //         {
+                  
+        //           meal: Food.meal,
+        //           netCarbs: Food.netCarbs.map
+        //         }
+        //         )
+        //     })
+        //   }
+        // )
       });
+      res.json(resObj)
     });
+  });
+}
+        
+      
+function getFood(foodArray){
+    //loop through dataValues and return an array of food
+    // [
+    //         {
+    //             meal: "",
+    //             netCarbs: 0
+    //         }
+    //     ]
+    return foodArray.map(({dataValues})=>{
+        return {
+            meal: dataValues.meal,
+            netCarbs: dataValues.netCarbs,
+        }
+    })
 }
 
-// app.get("/", function(req, res) {
-//     res.send("App works!!");
-// });
+
+
+
+
+
+//       .then(Food => {
+//         const resObj = Food.map(user => {
+//             return Object.assign(
+//                 {},{
+//                   name: Restaurant.name.map(food =>{
+//                       return Object.assign(
+//                           {},{
+//                               meal:Food.meal,
+//                               netCarbs: Food.netCarbs
+//                           }
+//                       )
+//                   })  
+//                 }
+//             )
+//         });
+      
+//     //   .then(function(results) {
+//     //     res.json(results);
+//     //   })
+//     //   .catch(function(err){
+//     //       res.json(err);
+//     //   });
+//     // });
+//       });
+
+// // app.get("/", function(req, res) {
+// //     res.send("App works!!");
+// // });
+
+// include: [{
+//     model: db.Food,
+// }]
